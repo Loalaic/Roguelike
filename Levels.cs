4aead3d7a9bb6cc22e0126b802679a23a0461f26
Loalaic <jjissuck@gmail.com>
2020-03-08 22:38:36 +0700

    Update Levels.cs

--- Levels.cs
+++ Levels.cs
@@ -9,39 +9,58 @@ namespace Roguelike
         private int dLevel = 0;
         private Map[] maps;
         private VisibleMap[] vmaps;
-        
-        public Levels(int level){
+
+        public Levels(int level)
+        {
             dLevel = level;
             maps = new Map[dLevel];
             vmaps = new VisibleMap[dLevel];
             dLevel = 0;
         }
-        
-        public void InitLevels(){
-            for (int i = 0; i < Map.Length(); i++){
-                maps[i] = new Map(120,29);
-                maps[i].GenerateDungeonRooms(5+(i/10));
+
+        public void InitLevels()
+        {
+            for (int i = 0; i < maps.Length; i++)
+            {
+                maps[i] = new Map(120, 29);
+                maps[i].GenerateDungeonRooms(5 + (i / 10));
                 maps[i].GenerateDungeonDoors();
                 maps[i].GenerateDungeonPaths();
                 maps[i].RemoveExcessiveDoors();
                 maps[i].Colorize();
+                vmaps[i] = new VisibleMap(120, 29);
             }
         }
-        
-        public Map getCurrentMap(){
+
+        public Map getCurrentMap()
+        {
             return maps[dLevel];
         }
-        
-        public VisibleMap getCurrentVisibleMap(){
+
+        public VisibleMap getCurrentVisibleMap()
+        {
             return vmaps[dLevel];
         }
-        
-        public void goDownStairs(){
+
+        public void goDownStairs()
+        {
             dLevel++;
         }
-        
-        public void goUpStairs(){
+
+        public void goUpStairs()
+        {
             dLevel--;
+        }
+
+        public int getDLevel()
+        {
+            return dLevel;
+        }
+
+        public void setDLevel(int dLevel)
+        {
+            this.dLevel = dLevel;
         }
     }
 }
+
